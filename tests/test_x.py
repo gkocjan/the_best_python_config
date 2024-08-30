d032c91b4f9570a0ae0eb0445159f0432bdc68b0
Grzegorz Kocjan <1679285+gkocjan@users.noreply.github.com>
2024-08-30 23:38:29 +0000

    Add db.host setting

--- tests/test_x.py
+++ tests/test_x.py
@@ -1,8 +1,21 @@
-from pydantic_settings import BaseSettings
+import pytest
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class DBConfig(BaseSettings):
+    host: str
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_nested_delimiter="__")
+
     debug: bool = False
+    db: DBConfig
+
+
+@pytest.fixture(autouse=True)
+def _default_settings(monkeypatch):
+    monkeypatch.setenv("DB__HOST", "default")
 
 
 def test_setting_DEBUG_env_sets_debug_to_True(monkeypatch):
@@ -11,3 +24,11 @@ def test_setting_DEBUG_env_sets_debug_to_True(monkeypatch):
     settings = Settings()
 
     assert settings.debug is True
+
+
+def test_setting_DB_HOST_sets_database_host_of_db_config(monkeypatch):
+    monkeypatch.setenv("DB__HOST", "not_local")
+
+    settings = Settings()
+
+    assert settings.db.host == "not_local"
